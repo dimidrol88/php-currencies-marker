ffbba63aad8718c7c58673ba4d673104c0915284
DimidroL <kuznecov_da@inbox.ru>
2018-04-13 16:55:02 +0500

    Added CurrencyMarker class

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # php-currencies-marker
 A simple, object-oriented class for obtaining exchange rates (сbr.ru)
+
+Installation
+
+Install with composer:
+
+composer require dimidrol88/php-curencies-marker
+
+Usage
+
+$valute = new CurrencyMarker();
+echo $valute->USD;
+echo $valute->EUR;
---
+++ src/CurrencyMarker.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace dimidrol88\phpCurenciesMarker;
+
+/**
+ * 
+ * Description of CurrencyMarker
+ * Simple php class
+ * @author dimidrol88
+ */
+class CurrencyMarker {
+
+    /**
+     * 
+     * @var array
+     */
+    public $curencies = [];
+
+    /**
+     * 
+     * @var string - Separator of integer and decimal parts
+     */
+    protected $separator = '.';
+
+    /**
+     * 
+     * @var string
+     */
+    protected $link = 'http://cbr.ru/scripts/XML_daily.asp?date_req=';
+
+    /**
+     * 
+     * @param string $date
+     * @param boolean $format - Use to format the exchange rate (true = rounding up, 2 decimal places)
+     */
+    public function __construct($date = null, $format = true, $separator = '.') {
+	$this->link = $this->getDate((string) $date);
+	$this->separator = $separator;
+	$this->curencies = $this->setСurrencies($format);
+	
+    }
+
+    /**
+     * 
+     * return float | string
+     */
+    public function __get($charCode) {
+	return (float)$this->curencies[(string) $charCode] ? (float)$this->curencies[(string) $charCode] : 'Курс валюты не найден! Проверьте символьный код!';
+    }
+
+    /**
+     * 
+     * @param boolean $format
+     * @return array|false
+     */
+    private function setСurrencies($format) {
+	$currencies = simplexml_load_file($this->link);
+	foreach ($currencies->xpath('//Valute') as $currency) {
+	    if (!$format) {
+		$currencies[(string) $currency->CharCode] = (float) str_replace(',', $this->separator, $currency->Value);
+	    } else {
+		$currencies[(string) $currency->CharCode] = round((float) str_replace(',', $this->separator, $currency->Value), 2, PHP_ROUND_HALF_UP);
+	    }
+	}
+	return $currencies ? $currencies : false;
+    }
+
+    /**
+     * 
+     * @param string $date
+     * @return string
+     */
+    private function getDate($date) {
+	return $date ? $this->link . $date : $this->link . (string) date("d/m/Y");
+    }
+
+}
